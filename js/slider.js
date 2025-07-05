bb6dccd927ed268021bdb42bc600bbd4b1928957
Virapub <virapublishers@gmail.com>
2025-07-05 11:04:17 +0530

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -3,11 +3,10 @@ window.addEventListener("DOMContentLoaded", () => {
   const slideContainer = document.getElementById("slides");
 
   const bannerImages = [
-    "images/slide1.jpg",
-    "images/slide2.jpg",
-    "images/slide3.jpg",
-    "images/slide4.jpg",
-    "images/slide5.jpg"
+    "images/slide6.jpg",
+    "images/slide7.jpg",
+    "images/slide8.jpg",
+    
   ];
 
   bannerImages.forEach((src, index) => {
