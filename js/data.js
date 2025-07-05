5f60dfa7d9c01ccea9699c3193d2343c84cb63b1
Virapub <virapublishers@gmail.com>
2025-07-05 07:46:41 +0530

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,14 +1,15 @@
-// js/data.js
-
+// data.js
 const products = [
   {
     id: "collapsible-electric-kettle",
     name: "Collapsible Electric Kettle",
     description: "Space-saving collapsible kettle with auto shut-off and boil-dry protection",
     priceINR: 1499,
+    priceUSD: 17.51,
     category: "Appliances",
     link: "https://amzn.to/448SSZx",
     image: "https://m.media-amazon.com/images/I/61WQVNM0LCL._SL1500_.jpg",
+    page: "products.html#collapsible-electric-kettle",
     rating: 4.5,
     features: ["Collapsible design", "800W power", "BPA-free materials"]
   },
@@ -17,9 +18,11 @@ const products = [
     name: "Automatic Soap Dispenser",
     description: "Touchless soap dispenser with infrared sensor for hygienic use",
     priceINR: 1388,
+    priceUSD: 16.21,
     category: "Smart Gadgets",
     link: "https://amzn.to/46sLIRc",
     image: "https://m.media-amazon.com/images/I/61Vqx4KEHuL._SL1024_.jpg",
+    page: "products.html#automatic-soap-dispenser",
     rating: 4.2,
     features: ["Touchless operation", "Battery powered", "Adjustable soap volume"]
   },
@@ -28,9 +31,11 @@ const products = [
     name: "Magnetic Fridge Shelf Rack",
     description: "Extra storage space that attaches magnetically to your fridge",
     priceINR: 1199,
+    priceUSD: 14.00,
     category: "Storage",
     link: "https://amzn.to/44rEZEO",
     image: "https://m.media-amazon.com/images/I/81gCEjDOpSL._SL1500_.jpg",
+    page: "products.html#magnetic-fridge-shelf",
     rating: 4.3,
     features: ["Strong magnets", "Adjustable width", "Easy to clean"]
   }
