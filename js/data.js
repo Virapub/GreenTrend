8e412228420b2d7271835d6aa84a0260344a13f6
Virapub <virapublishers@gmail.com>
2025-07-18 17:02:03 +0530

    Update data.js

--- js/data.js
+++ js/data.js
@@ -2,7 +2,8 @@ const products = [
     {
         id: 1,
         name: "Smart Wi-Fi Coffee Maker",
-        description: "Wake up to fresh coffee with this app-controlled coffee maker. Schedule brews, adjust strength, and more from your phone. Enjoy seamless integration with your smart home.Brewing a great cup of coffee at home is easy with the Solo Single-Serve 2-in-1 Coffee Maker. It can handle both K-Cup® pods and your own grounds in the included reusable pod with handle. When placed in the Instant Solo, the reusable pod activates a unique brewing sequence that extracts more flavor, aroma and body from your favorite ground coffee. Choose from multiple brew sizes with 8, 10 or 12 oz cups, and adjust the brewing strength to make it as bold as you like. The 40oz reservoir means you can refill less frequently, and the removable drip tray lets you fill up to a 7" tall 16oz travel mug.",
+        description: "Wake up to fresh coffee with this app-controlled coffee maker. Schedule brews, adjust strength, and more from your phone. Enjoy seamless integration with your smart home.
+        Brewing a great cup of coffee at home is easy with the Solo Single-Serve 2-in-1 Coffee Maker. It can handle both K-Cup® pods and your own grounds in the included reusable pod with handle. When placed in the Instant Solo, the reusable pod activates a unique brewing sequence that extracts more flavor, aroma and body from your favorite ground coffee. Choose from multiple brew sizes with 8, 10 or 12 oz cups, and adjust the brewing strength to make it as bold as you like. The 40oz reservoir means you can refill less frequently, and the removable drip tray lets you fill up to a 7" tall 16oz travel mug.",
         image: "https://m.media-amazon.com/images/I/71b4ReFYVrL._AC_SX679_.jpg",
         category: "mid-range",
         priceUSD: 89.99,
